27359bc3cc6766dc675ab9ddb250f89c5b04f136
ringoshu <alan900000@gmail.com>
2021-06-12 15:38:13 -0400

    .

--- src/art.js
+++ src/art.js
@@ -142,6 +142,70 @@ const art17 = {
   "imageShape": "landscape",
   "includeingallery": true,
 }
+const art20 = {
+  "link": "https://farm3.static.flickr.com/2801/4276536239_d9af88850d_o.jpg",
+  "title": 'Blindfaith',
+  "media": 'At Exit Art (as part of the Performance in Crisis series), New York, NY',
+  "statement": 'Multimedia live performance. A live goldfish in a clear glass bowl is placed on a pedestal at each end of the space. Two performers mirror the same action repeatedly: one takes a cup of water from bowl A and pours it into bowl B, and the other does the opposite. In the center, the third performer walks in a large circle repeatedly. She is blindfolded and carries a live fish in a bowl. She dips her feet in this bowl making her footprints on the concrete floor. A 5 minute audio-visual file is projected on a wall and repeated throughout.',
+  "imageShape": "landscape",
+  "includeingallery": true,
+}
+const art21 = {
+  "link": "https://farm5.static.flickr.com/4006/4274235477_2d2f024563_o.jpg",
+  "title": 'Polkadot',
+  "media": 'At Mazer Theater at Education Alliance, New York, NY USA',
+  "statement": 'Interdisciplinary performance art.',
+  "imageShape": "landscape",
+  "includeingallery": true,
+}
+const art22 = {
+  "link": "https://farm3.static.flickr.com/2636/3724838050_01b6514f2c_o.jpg",
+  "title": 'kasa-casa',
+  "media": 'Funded by NYC Department of Cultural Affairs, administered by Lower Manhattan Cultural Council',
+  "statement": 'Interdisciplinary performance art presented at IS143 in Washington Heights, New York.',
+  "imageShape": "landscape",
+  "includeingallery": true,
+}
+const art23 = {
+  "link": "https://farm4.static.flickr.com/3456/3717957142_319d3a2cbf_o.jpg",
+  "title": 'linebreak',
+  "media": 'At 320 Studios, New York, NY USA',
+  "statement": 'Interdisciplinary performance comission for Financial Corps fundraising event. Inspirations: Intersection of various lives. Constrution and deconstruction.',
+  "imageShape": "landscape",
+  "includeingallery": true,
+}
+const art24 = {
+  "link": "https://farm3.static.flickr.com/2493/3720653581_4390f20fdf_o.jpg",
+  "title": 'fella',
+  "media": 'at the virginia center for the creative arts, amherst, va, usa',
+  "statement": 'outdoor interdisciplinary performance.',
+  "imageShape": "landscape",
+  "includeingallery": true,
+}
+const art25 = {
+  "link": "https://farm3.static.flickr.com/2610/3714779488_d39cefba40_o.jpg",
+  "title": 'glimmer',
+  "media": 'at i-park, east haddam, ct',
+  "statement": 'outdoor installation and performance in the forest.',
+  "imageShape": "landscape",
+  "includeingallery": true,
+}
+const art26 = {
+  "link": "https://farm4.static.flickr.com/3433/3720893965_197612253f_o.jpg",
+  "title": 'tsumiki',
+  "media": 'at exit art, nyc. live audience performance project',
+  "statement": 'repeating continuously during the exhibition, i build a meticulously perfect geometric structure that surrounds me and appears to trap me inside. once the structure is complete, i come out of it at once using only the force of my body. once outside of the structure, i rush to clean it up and start building it again from the dismantled structure. each construction takes about 15-20 minutes.',
+  "imageShape": "landscape",
+  "includeingallery": true,
+}
+const art27 = {
+  "link": "https://farm3.static.flickr.com/2512/3724739538_b4b96e75d5_o.jpg",
+  "title": 'seesaw',
+  "media": 'at exit art, nyc. live audience performance project',
+  "statement": 'live performance in the window space on busy street in nyc. i walked extremely slow motion as in buddhist meditation (3-4 min. to cross 8 feet seesaw) back and forth on top of seesaw for the 3 day group performance event',
+  "imageShape": "landscape",
+  "includeingallery": true,
+}
 
 const art = [
     art0,
@@ -162,6 +226,14 @@ const art = [
     art15,
     art16,
     art17,
+    art20,
+    art21,
+    art22,
+    art23,
+    art24,
+    art25,
+    art26,
+    art27,
 ]
 
 export default art
